b3d920cda919a3927420880bd76ffc625db502ce
kmilewczyk96 <kmilew0802@gmail.com>
2024-12-29 20:57:30 +0100

    Marquee animation.

--- front/src/components/atoms/Marquee.tsx
+++ front/src/components/atoms/Marquee.tsx
@@ -17,24 +17,31 @@ export default function Marquee({className, elementsToDisplay}: IProps): ReactEl
   const marqueeWrapper = useRef(null);
   const [animationSpeed, setAnimationSpeed] = useState(0);
 
+  let content: ReactElement =
+    <>
+      {elementsToDisplay.map((el, index) => (
+        <li key={index}>{el}</li>
+      ))}
+    </>
+
+
   useLayoutEffect(() => {
+    console.log("triggered!")
     // @ts-ignore
     const marqueeWidth: number = marqueeWrapper.current.offsetWidth;
     // @ts-ignore
     if (marqueeWidth > marqueeWrapper.current.parentElement.offsetWidth) {
-      setAnimationSpeed(marqueeWidth / 5);
+      setAnimationSpeed(marqueeWidth / 250);
     }
   }, [])
 
   return (
-    <ul ref={marqueeWrapper} className={[styles.wrapper, className].join(" ")}>
-      {
-        animationSpeed === 0 ? (elementsToDisplay.map(
-          (el, index) => <li key={index}>{el}</li>
-        )): (elementsToDisplay.map(
-          (el, index) => <li key={index}>{el}</li>
-        ))
-      }
+    <ul
+      ref={marqueeWrapper}
+      style={{animationDuration: `${animationSpeed}s`}}
+      className={[styles.wrapper, className].join(" ")}
+    >
+      {animationSpeed === 0 ? content : [content, content]}
     </ul>
   );
 }
