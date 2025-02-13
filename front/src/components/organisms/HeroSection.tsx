a37146207e1179057a11472be690287faefca0f1
kmilewczyk96 <kmilew0802@gmail.com>
2025-02-13 18:32:26 +0100

    Final media queries for Hero section. 'All' devices covered.

--- front/src/components/organisms/HeroSection.tsx
+++ front/src/components/organisms/HeroSection.tsx
@@ -24,35 +24,34 @@ const HeroSection: ForwardRefExoticComponent<RefAttributes<HTMLElement>> = forwa
 
     return (
       <Section sectionID={"home"} ref={ref} className={styles.heroSection}>
-        <>
-          <div className={[styles.contentWrapper, "sectionInlineWrapper"].join(" ")}>
-            <div className={styles.personalInfoWrapper}>
-              <p className={styles.title}>Python Developer</p>
-              <GlitchText
-                text={"Karol Milewczyk"}
-                htmlTextTag={htmlTextTags.h2}
-                className={styles.name}
-                animationDelaySeconds={3}
-              />
-              <p className={styles.description}>
-                Hi! I am a Software Developer proficient with Python and TypeScript.<br/>
-                I have experience with libraries such as Django, DRF, FastAPI, PyQt and React.
-                I'm strongly focused on applying best practices, writing clean and maintainable code.
-                I'm always eager to broaden my skills (both technical and soft).<br/>
-                I hope you enjoy browsing my website and consider filling contact form attached at the very end of the page.
-              </p>
-              <Button className={styles.cta} onClick={handleClick}>Learn More</Button>
-            </div>
-            <div className={styles.photoWrapper}>
-              <GlitchImage
-                imageSource={myPhoto}
-                imageAltText={"My photo."}
-                className={styles.photo}
-                animationDelaySeconds={3}
-              />
-            </div>
+        <div className={[styles.contentWrapper, "sectionInlineWrapper"].join(" ")}>
+          <div className={styles.personalInfoWrapper}>
+            <p className={styles.title}>Python Developer</p>
+            <GlitchText
+              text={"Karol Milewczyk"}
+              htmlTextTag={htmlTextTags.h2}
+              className={styles.name}
+              animationDelaySeconds={3}
+            />
+            <p className={styles.description}>
+              Hi! I am a Software Developer proficient with Python and TypeScript.
+              I have experience with libraries such as Django, DRF, FastAPI, PyQt and React.
+              I'm strongly focused on applying best practices, writing clean and maintainable code.
+              I'm always eager to broaden my skills (both technical and soft).
+              I hope you enjoy browsing my website and consider filling contact form attached at the very end of the
+              page.
+            </p>
+            <Button className={styles.cta} onClick={handleClick}>Learn More</Button>
           </div>
-        </>
+          <div className={styles.photoWrapper}>
+            <GlitchImage
+              imageSource={myPhoto}
+              imageAltText={"My photo."}
+              className={styles.photo}
+              animationDelaySeconds={3}
+            />
+          </div>
+        </div>
       </Section>
     );
   }
