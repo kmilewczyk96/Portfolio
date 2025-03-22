b844eb75347f8730d957021374c9bad822745304
kmilewczyk96 <kmilew0802@gmail.com>
2025-03-22 16:54:12 +0100

    ContactCard.tsx update, it now contains photo and labels for socials. Webp format for background and contact card images.

--- front/src/components/molecules/ContactCard.tsx
+++ front/src/components/molecules/ContactCard.tsx
@@ -12,16 +12,21 @@ interface IProps {
 export default function ContactCard({className}: IProps): ReactElement {
   return (
     <div className={[styles.wrapper, className].join(" ")}>
-      <div className={styles.socials}>
-        <SvgLink goTo={"https://www.linkedin.com/in/karol-milewczyk/"} icon={socialIcons.linkedin}/>
-        <SvgLink goTo={"https://github.com/kmilewczyk96/"} icon={socialIcons.github}/>
+      <div className={styles.contactHeader}>
+        <div className={styles.headerPhotoWrapper}>
+          <img className={styles.headerPhoto} src={"/src/assets/headerPhoto.webp"} alt={"Header photo of myself."}/>
+        </div>
+        <line/>
+        <div className={styles.socials}>
+          <SvgLink goTo={"https://www.linkedin.com/in/karol-milewczyk/"} icon={socialIcons.linkedin} label={"LinkedIn"}/>
+          <SvgLink goTo={"https://github.com/kmilewczyk96/"} icon={socialIcons.github} label={"Github"}/>
+        </div>
       </div>
       <div className={styles.content}>
         <p>
-          Currently I am searching for position as Backend/Full-stack developer, I can also help you create website
-          like this one as a freelancer.<br/>
+          Currently I am looking for position of Backend/Full-stack developer, I can also help you create websites
+          (like this one) as a freelancer.<br/>
           If you have any questions, please fill out attached form, and I will get back to you within 48 hours.
-          Feel free to add me to your LinkedIn network or explore my GitHub repositories using the links above.
         </p>
       </div>
       <div className={styles.statusWrapper}>
